50964387ab0c4fc9bfc27e20d0457124c27e2701
Елисей Ехлаков <elisey.ekhlakov@krit.pro>
2020-12-28 16:40:27 +0400

    lesson91

--- src/index.js
+++ src/index.js
@@ -1,13 +1,50 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/app/';
+// import App from './components/app/';
+class WhoAmI extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      years: 26,
+      nation: 'ru'
+    }
+    // this.nextYear = this.nextYear.bind(this);
+    this.nextYear = () => {
+      
+    }
+  }
+  // nextYear() {
 
+  //   this.setState(state => ({
+  //     years: ++state.years
+  //   }));
+  // }
+  render() {
+    const {name, surname, link} = this.props;
+    const {years} = this.state;
+    return (
+      <>
+      <button onClick={this.nextYear} >++</button>
+      <h1>My name is {name}, surname - {surname}, years = {years}</h1>
+      <a href={link}>My profile</a>
+      </>  
+    )  
+  }
+}
+
+
+const All = () => {
+  return (
+    <>
+<WhoAmI name="John" surname="Owen" link="facebook.com" />
+<WhoAmI name="Ivan" surname="Owen" link="facebook.com" />
+<WhoAmI name="Peter" surname="Owen" link="facebook.com" />
+    </>
+  )
+}
 
 ReactDOM.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>,
-  <App/>,  document.getElementById('root')
+  <All />,  document.getElementById('root')
 );
 
